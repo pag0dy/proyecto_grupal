e323d9c3c35401d377e5f1bc568891bc5cc7650c
Agustin <agustin.andrews@gmail.com>
2021-04-22 15:24:21 -0400

    Agrega modelos para reapps: gistro_acceso y campaÃña

--- proyecto_grupal/apps/campana/models.py
+++ proyecto_grupal/apps/campana/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from ..registro_acceso.models import *
 
-# Create your models here.
+class Campana(models.Model):
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField(max_length=1000)
+    fecha_limite = models.DateTimeField()
+    # pocentaje = ?
+    agrupacion = models.ForeignKey(Agrupacion, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Recurso(models.Model):
+    tipo = models.CharField(max_length=60)
+    cantidad = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Aporte(models.Model):
+    tipo = models.CharField(max_length=60)
+    estado = models.CharField(max_length=45)
+    campana = models.ForeignKey(Campana, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    recurso = models.ForeignKey(Recurso, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- proyecto_grupal/apps/registro_acceso/models.py
+++ proyecto_grupal/apps/registro_acceso/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class Region(models.Model):
+    region = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    rut = models.CharField(max_length=12)
+    email = models.EmailField(max_length=100)
+    # patrocinador = ?
+    password = models.CharField(max_length=80)
+    region = models.OneToOneField(Region, on_delete=models.CASCADE, primary_key=True,)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Agrupacion(models.Model):
+    nombre = models.CharField(max_length=150)
+    rut = models.CharField(max_length=12)
+    email = models.EmailField(max_length=100)
+    descripcion = models.TextField(max_length=1000)
+    password = models.CharField(max_length=80)
+    categoria = models.OneToOneField(Categoria, on_delete=models.CASCADE, primary_key=True,)
+    region = models.OneToOneField(Region, on_delete=models.CASCADE, primary_key=True,)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
