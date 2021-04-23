5c03456c82e736a81a59e9738d8d590d4eab2e17
Agustin <agustin.andrews@gmail.com>
2021-04-22 20:41:36 -0400

    actualizada os los modelos y creada la branch models

--- proyecto_grupal/apps/campana/models.py
+++ proyecto_grupal/apps/campana/models.py
@@ -3,24 +3,25 @@ from ..registro_acceso.models import *
 
 class Campana(models.Model):
     titulo = models.CharField(max_length=100)
-    descripcion = models.TextField(max_length=1000)
+    descripcion = models.TextField(max_length=750)
     fecha_limite = models.DateTimeField()
-    # pocentaje = ?
     agrupacion = models.ForeignKey(Agrupacion, on_delete=models.CASCADE)
+    # Falta incorporar un campo de porcentaje de cumplimiento de la meta de la campaña 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 class Recurso(models.Model):
-    tipo = models.CharField(max_length=60)
+    tipo = models.CharField(max_length=60) # Incorporar: choices
     cantidad = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 class Aporte(models.Model):
     tipo = models.CharField(max_length=60)
-    estado = models.CharField(max_length=45)
+    estado = models.CharField(max_length=45) # Este campo de estado no sería lo mismo que el campo aprobado? 
     campana = models.ForeignKey(Campana, on_delete=models.CASCADE)
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     recurso = models.ForeignKey(Recurso, on_delete=models.CASCADE)
+    aprobado = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
