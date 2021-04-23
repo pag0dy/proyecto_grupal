aa247f7584ad118294e6125928c63dd1edb767e2
Guerra-Defranchi <guerra.defranchi@protonmail.com>
2021-04-22 22:27:51 -0400

    Modificaiones a los modelos

--- proyecto_grupal/apps/campana/models.py
+++ proyecto_grupal/apps/campana/models.py
@@ -4,24 +4,17 @@ from ..registro_acceso.models import *
 class Campana(models.Model):
     titulo = models.CharField(max_length=100)
     descripcion = models.TextField(max_length=750)
-    fecha_limite = models.DateTimeField()
-    agrupacion = models.ForeignKey(Agrupacion, on_delete=models.CASCADE)
-    # Falta incorporar un campo de porcentaje de cumplimiento de la meta de la campaña 
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-class Recurso(models.Model):
-    tipo = models.CharField(max_length=60) # Incorporar: choices
-    cantidad = models.IntegerField()
+    fecha_limite = models.DateTimeField(blank=True, null=True)
+    agrupacion = models.ManyToManyField(Agrupacion, related_name='campana_activa')
+    recaudacion = models.PositiveSmallIntegerField()
+    meta = models.PositiveSmallIntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 class Aporte(models.Model):
-    tipo = models.CharField(max_length=60)
-    estado = models.CharField(max_length=45) # Este campo de estado no sería lo mismo que el campo aprobado? 
-    campana = models.ForeignKey(Campana, on_delete=models.CASCADE)
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
-    recurso = models.ForeignKey(Recurso, on_delete=models.CASCADE)
+    campana = models.ForeignKey(Campana, on_delete=models.CASCADE, related_name='aporte_ingresado')
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='aporte_usuario')
+    cantidad = models.PositiveSmallIntegerField()
     aprobado = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- proyecto_grupal/apps/registro_acceso/models.py
+++ proyecto_grupal/apps/registro_acceso/models.py
@@ -1,18 +1,17 @@
 from django.db import models
 
-class Region(models.Model):
-    nombre = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+# class Region(models.Model):
+    # nombre = models.CharField(max_length=100)
+    # created_at = models.DateTimeField(auto_now_add=True)
+    # updated_at = models.DateTimeField(auto_now=True)
 
 class Usuario(models.Model):
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
     rut = models.CharField(max_length=12)
     email = models.EmailField(max_length=100)
-    # patrocinador = ?
     password = models.CharField(max_length=80)
-    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+    # region = models.ForeignKey(Region, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -28,7 +27,7 @@ class Agrupacion(models.Model):
     email = models.EmailField(max_length=100)
     descripcion = models.TextField(max_length=750)
     password = models.CharField(max_length=80)
-    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
-    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name='agrupacion_relacionada')
+    # region = models.ForeignKey(Region, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
