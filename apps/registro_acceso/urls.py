526c2e360a2c2e99edf7eb60bf88e9e92cf7687a
Marita <maschmutzer@uc.cl>
2021-04-26 20:56:47 -0300

    agregar urls acceso_agrupacion

--- apps/registro_acceso/urls.py
+++ apps/registro_acceso/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
 path('registro',  views.registro, name="registro"),
 path('registro_agrupacion', views.registro_agrupacion, name="registro_agrupacion"),
 path('acceso', views.acceso, name="acceso"),
+path('acceso_agrupacion', views.acceso, name="acceso_agrupacion"),
 path('logout', views.logout, name="logout"),
 ]
 
