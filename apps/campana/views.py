6b64cd6f9d5a0563735494ba224f1d56b5547ee4
Marita <maschmutzer@uc.cl>
2021-04-27 18:37:02 -0300

    agrega aporte

--- apps/campana/views.py
+++ apps/campana/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.contrib import messages
-from .forms import FormularioCampana
+from .forms import FormularioCampana, FormularioAporte
 from .models import Campana, Agrupacion
 import bcrypt
 
@@ -12,7 +12,7 @@ def agregar_campana(request):
 		form = FormularioCampana(request.POST)
 		if form.is_valid():
 				nueva_campana = form.save(commit=False)
-				cdnueva_campana.agrupacion = Agrupacion.objects.get(id=request.session['idagrupacion'])
+				nueva_campana.agrupacion = Agrupacion.objects.get(id=request.session['idagrupacion'])
 				nueva_campana.save()
 				return redirect('/campana')
 		else:
@@ -26,4 +26,22 @@ def agregar_campana(request):
 		}		
 		return render(request, 'campana/registro.html', context)
 
-
+def agregar_aporte(request, id_campana):
+	if request.method == 'POST':
+		form = FormularioAporte(request.POST)
+		if form.is_valid():
+				nuevo_aporte = form.save(commit=False)
+				nuevo_aporte.campana = Campana.objects.get(id=id_campana)
+				nuevo_aporte.usuario = Usuario.objects.get(id=request.session['id'])
+				nuevo_aporte.save()
+				return redirect('/campana')
+		else:
+			context = {
+				'form': form
+			}
+			return render(request, 'campana/registro.html', context)
+	else:
+		context = {
+			'form' : FormularioAporte(),
+		}		
+		return render(request, 'campana/registro.html', context)
