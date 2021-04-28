590fdd56ed5158a72da1f25ca2b989103fc1f800
Agustin <agustin.andrews@gmail.com>
2021-04-28 01:31:41 -0400

    mejora la estetica de los formularios de registro

--- static/js/functions.js
+++ static/js/functions.js
@@ -1,11 +1,16 @@
     $(document).ready(function(){
         $('#toggle-btn').click(function(){
             $('#toggle1').attr('style', '');
+            $('#toggle-btn').addClass('btn-selected')
             $('#toggle2').attr('style', 'visibility: hidden;')
+            $('#toggle-btn2').removeClass('btn-selected')
+
         })
         $('#toggle-btn2').click(function(){
             $('#toggle1').attr('style', 'visibility: hidden;');
+            $('#toggle-btn').removeClass('btn-selected')
             $('#toggle2').attr('style', '')
+            $('#toggle-btn2').addClass('btn-selected')
         })
 
     })
